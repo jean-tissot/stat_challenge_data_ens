23206cb4a8cde1b7aa369216727d5dae655ab58f
Jean Tissot <asphjt@gmail.com>
2021-01-19 18:13:22 +0100

    train sur fenêtre aléatoire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from treat_data import datatreat
+from treat_data import data_treat_vector, data_treat_alea_window
 from train_model import train_lstm_model
 
-x_test, x_train, y_train, N_test, N_train, data_shape = datatreat()
+x_test, x_train, y_train, N_test, N_train, data_shape = data_treat_alea_window()
 
 model=train_lstm_model(data_shape, x_train, y_train)
--- treat_data.py
+++ treat_data.py
@@ -1,7 +1,8 @@
 from read_data import dataread
 from utils import vector_generator
+from random import randint
 
-def datatreat():
+def data_treat_vector():
     x_test, x_train, y_train, N_test, N_train = dataread()
 
     # treat data here (create a vector with data)
@@ -13,3 +14,12 @@ def datatreat():
     return x_test, x_train, y_train, N_test, N_train, data_shape
 
 #éventuellement créer autant d'autre fonction qu'il y a de méthode de traitement à tester
+
+def data_treat_alea_window():
+    x_test, x_train, y_train, N_test, N_train = dataread()
+    x_test_alea=[]
+    shape=x_train.shape
+    for data in x_train:
+        x_test_alea.append(data[randint(0,shape[1]-1)])
+    return x_test, x_test_alea, y_train, N_test, N_train, shape[2:]
+
