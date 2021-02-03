ad8d0e54375dc0f8ce1cb60a7c98d52e8a57e5fc
Jean Tissot <asphjt@gmail.com>
2021-02-03 12:47:21 +0100

    update main_J

--- main_J.py
+++ main_J.py
@@ -3,14 +3,15 @@ from test import test_1,test_2
 from models import model_of_test, lstm_model
 from tools import plot_loss_acc_history
 
+id='lstm'
+epochs=80
+batch_size=100
+preprocess=None
+balancing_method='SMOTE'
+validation_split=0.1
 treat_function = datatreat_J1
 my_model = lstm_model
 test_fonction = test_2
-epochs=80
-batch_size=100
-id='test'
-preprocess=None
-balancing_method='SMOTE'
 
 print("loading data...")
 x, y, x_final = dataread()
@@ -22,10 +23,10 @@ print("\nforme des données d'entrée: ", x_train[0].shape)
 model = my_model(x_train[0].shape)
 model.summary()
 print("\ntraining model...")
-history=model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.1)
+history=model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
 
 print("testing model...")
-accuracy, roc, f1_macro, f1_wei = test_fonction(model, x_test, y_test, id)
+accuracy, roc, f1_macro, f1_wei = test_fonction(model, x_test, y_test, validation_split)
 
 #print("results (accuracy, roc, f1_macro, f1_wei):", accuracy, roc, f1_macro, f1_wei)
 plot_loss_acc_history(history, id, 0.1)
